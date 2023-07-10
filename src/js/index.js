ea030250fd12f0bb43875ff1233402ebf607fa7b
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-07-10 16:53:00 +0000

    browser router for index.js and navbar component

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,18 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home';
+import Navbar from './component/navbar';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
-
-//import your own components
-import Home from "./component/home.jsx";
-
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+      ReactDOM.render(
+        <React.StrictMode>
+          <Router>
+          <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/navbar" element={<Navbar />} />
+          </Routes>
+            {/* Define other routes here */}
+          </Router>
+        </React.StrictMode>,
+        document.getElementById('app')
+      );
