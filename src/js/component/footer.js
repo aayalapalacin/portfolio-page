c649df66a6dd9c07fa4e9394e45d831b106cd1f4
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-07-20 22:32:53 +0000

    footer structure set

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,13 +1,17 @@
 import React from "react";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBlenderPhone, faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 //include images into your bundle
-
 //create your first component
 const Footer = () => {
 	return (
 		<nav className="navbar bg-light">
   <form className="container-fluid justify-content-center">
-    <button className="btn btn-sm btn-outline-secondary" type="button">Smaller button</button>
+  <FontAwesomeIcon icon={faLinkedin} />
+  <FontAwesomeIcon icon={faGithub} />
+  <FontAwesomeIcon icon={faEnvelope} />
+  <FontAwesomeIcon icon={faPhone} />
   </form>
 </nav>
 	);
