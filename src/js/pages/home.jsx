94d67bdaa708e078fa0df7427539f9f5c789d0b7
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-07-20 22:12:52 +0000

    home structure set

--- src/js/pages/home.jsx
+++ src/js/pages/home.jsx
@@ -7,8 +7,20 @@ import "../../styles/index.css"
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center ">
-			
+		<div className="container ">
+			<div className="row">
+				<div className="col "> 
+					<div className=" home-name row bg-secondary">row1</div>
+					<div className=" home-title row">row2</div>
+					<div className=" home-music-player row">row2</div>
+				</div>
+				<div className="col">
+					<div className="row">
+						<div className=" home-img col-5 bg-secondary">row1</div>
+						<div className=" home-prog-languages col-7 bg-danger">row2</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
