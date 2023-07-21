8aa873c08cff30d136c3f096e99e6e707eb87cfe
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-07-21 17:34:30 +0000

    about structure set

--- src/js/pages/about.js
+++ src/js/pages/about.js
@@ -5,8 +5,20 @@ import React from "react";
 //create your first component
 const About = () => {
 	return (
-		<div>about</div>
+		<div className="container">
+			<div className=" top-row d-flex justify-content-around">
+				<div className="about-work">work</div>
+				<div className="about-fun-fact">fun fact</div>
+			</div>
+			<div className=" mid-row  text-center">
+				<div className="about-photo">photo</div>
+			</div>
+			<div className=" low-row d-flex justify-content-around">
+				<div className="  about-culture">culture</div>
+				<div className="  about-arts">arts</div>
+			</div>
+		</div>
 	);
-};
+}; 
 
 export default About;
