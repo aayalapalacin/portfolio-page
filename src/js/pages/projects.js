94287ec132bf0aac03f3eae3aa2ffab0daacf4d0
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-07-23 19:06:10 +0000

    looping projects object rendering all info, display set

--- src/js/pages/projects.js
+++ src/js/pages/projects.js
@@ -13,28 +13,28 @@ const Projects = () => {
 		"languages":["HTML","CSS","REACT","BOOTSTRAP","JAVASCRIPT", "NPM PROGRESS BAR"],
 		"link":"https://levis-the-archive.vercel.app/",
 		"code":"https://github.com/aayalapalacin/theArchive",
-		"photo":{archive}
+		"photo":archive
 	},
 	{"title":"Ambee",
 	"description":"Mobile app to help users find new movie to watch based on genre, process of elimination with tinder swiping feature, and movies available on netflix via Movies of the Night API.",
 	"languages":["REACT NATIVE","MOVIES OF THE NIGHT API","REACT NATIVE PAPER","EXPO GO"],
 	"link":"https://expo.dev/@alexayala91/Ambee?serviceType=classic&distribution=expo-go",
 	"code":"https://github.com/aayalapalacin/Ambee",
-	"photo":{ambee}
+	"photo":ambee
 },
 {"title":"Miami Dade College CNA Page",
 "description":"Miami Dade College website for nursing students executed with clean and readable code in HTML. Clean and professional look with bootstrap and CSS styling, and dynamic with media queries for all screen sizes",
 "languages":["HTML","CSS","JQUERY","BOOTSTRAP","JAVASCRIPT"],
 "link":"https://ce.mdc.edu/contentManagement.do?method=load&code=CM000007",
 "code":"n/a",
-"photo":{cna}
+"photo":cna
 },
 {"title":"Miami Dade College Skillshop Page",
 "description":"Miami Dade College website for enrolled sign up for courses. executed with clean and readable code in HTML. Clean and professional look with bootstrap and CSS styling, and dynamic with media queries for all screen sizes.",
 "languages":["HTML","CSS","JQUERY","BOOTSTRAP","JAVASCRIPT"],
 "link":"https://www.mdc.edu/skillshop/",
 "code":"n/a",
-"photo":{skillshop}
+"photo":skillshop
 },
 ]
 	return (
@@ -45,28 +45,40 @@ const Projects = () => {
 				<div className=" projects-photo row ">row1</div>
 				<div className=" projects-list row">row2</div>
 			</div>
-			<div className="project-content col-8 ">{projectContent.map((item,i)=>{
-				item.languages.forEach((item) =>{
-					console.log(item)
-				})
+			<div className="project-content col-8 bg-secondary ">{projectContent.map((item,i)=>{
+				
 				return(
-					<div key={i}>
-						<h5>Title:</h5> {item.title}
-						<h5>Description: </h5>{item.description}
-						<h5>Languages/Libraries:</h5>
-						
-						{item.languages.map((language,i) =>{
-							return(
-								<div className="d-flex" key={i}>
-									<p>
-									{language}
-									</p>
-								</div>
-							);
-						})}
-					
-						<h5>Link: <a href={item.link} target="_blank">live link</a></h5>
-						<h5>Code:</h5>{item.code}
+					<div className="d-flex align-items-center " key={i}>
+						<div className=" col-6 project-content-date">
+
+							<div className=" d-flex project-title">
+								<h5>Title:</h5> {item.title}
+							</div>
+							<div className=" d-flex project-description">
+								<h5>Description: </h5>{item.description}
+							</div>
+							<div className=" d-flex project-languages">
+								<h5>Languages/Libraries:</h5>
+								{item.languages.map((language,i) =>{
+									return(
+										<div className="d-flex" key={i}>
+											<p>
+											{language}
+											</p>
+										</div>
+									);
+								})}
+							</div>
+							<div className=" d-flex project-link">
+								<h5>Link: <a href={item.link} target="_blank">live link</a></h5>
+							</div>
+							<div className=" d-flex project-code">
+								<h5>Code:</h5>{item.code}
+							</div>
+						</div>
+						<div className="project-content-photo text-center col-6">
+								<img className="w-75" src={item.photo} alt={item.title}/>
+						</div>
 					</div>
 				)
 			})}</div>
