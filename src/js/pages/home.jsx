c56d20475a339571539a17384559a04852c61501
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-09-24 23:35:30 +0000

    added skip button functionality to audio player

--- src/js/pages/home.jsx
+++ src/js/pages/home.jsx
@@ -1,8 +1,9 @@
-import React, {  useState } from "react";
+import React, { useState } from "react";
 // audio player imports
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css'
 import sonrisa from "../../assets/audio/tu-sonrisa.mp3"
+import precious from "../../assets/audio/Precious.mp3"
 
 import "../../styles/index.css"
 
@@ -23,14 +24,15 @@ import sql from "../../assets/img/prog-languages/sql.png"
 import point from "../../assets/img/portrait/point.png"
 
 const Home = () => {
-	const [showPopover,setShowPopver]=useState(false)
+	const [showPopover, setShowPopver] = useState(false)
+	const [songIndex, setSongIndex] = useState(0)
 	let imgArray = [javascript, boostrap, reactNative, css, react, html, jekyll, materialUi, python, reactNativePaper, sql]
-
+	let songArray = [sonrisa, precious]
 	return (
 		<div className="container bg-faint-blue rounded py-3 px-4">
 			<div className="row">
 				<div className="col ">
-					<div className="name-title-desc-container border rounded  p-3 "> 
+					<div className="name-title-desc-container border rounded  p-3 ">
 						<div className=" home-name row ">
 							<h2>Alexander Ayala Palacin</h2>
 
@@ -48,22 +50,39 @@ const Home = () => {
 
 					</div>
 					<div className=" home-music-player row mt-5">
-					
 						<h5 className="mb-3 ms-1">Original Music</h5>
 						<div className="audioPopver d-flex">
 
-							<div className="w-50" 	
-							onMouseEnter={()=>setShowPopver(!showPopover)}
-							onMouseLeave={()=>setShowPopver(!showPopover)}
+							<div className="w-50"
+								onMouseEnter={() => setShowPopver(!showPopover)}
+								onMouseLeave={() => setShowPopver(!showPopover)}
 							>
-									
+
 								<AudioPlayer
-							
-									src={sonrisa}
+									showJumpControls={false}
+									showSkipControls
+									onClickNext={(e) => {
+										if (songIndex != songArray.length - 1) {
+											setSongIndex(songIndex + 1)
+										}
+										else {
+											setSongIndex(0)
+										}
+									}}
+									onClickPrevious={(e) => {
+										console.log(songIndex, "previous")
+										if (songIndex != 0) {
+											setSongIndex(songIndex - 1)
+										}
+										else {
+											setSongIndex(songArray.length - 1)
+										}
+									}}
+									src={songArray[songIndex]}
 									className="audio"
 								/>
 							</div>
-							<div className={` ${showPopover ? "d-block" : "d-none"} popover bg-info w-25 text-center m-2`} style={{boxShadow:"0px 1px 7px 0px white"}}>
+							<div className={` ${showPopover ? "d-block" : "d-none"} popover bg-info w-25 text-center m-2`} style={{ boxShadow: "0px 1px 7px 0px white" }}>
 								<div className="p-2 "> Recordings of my original music </div>
 							</div>
 						</div>
@@ -74,32 +93,32 @@ const Home = () => {
 						<div className=" home-img col-5">
 							<img
 								className=" position-relative w-100 "
-								src={point} alt="point" 
-							style={{
-								bottom: "72px"
-							}}
+								src={point} alt="point"
+								style={{
+									bottom: "72px"
+								}}
 							/>
 						</div>
 						<div className=" home-prog-languages col-7 d-flex align-items-center ">
-							<div id="carouselExampleFade" data-bs-ride="carousel" className="carousel slide carousel-fade ">
+							<div id="programmingLanguage" data-bs-ride="carousel" className="carousel slide carousel-fade ">
 								<div className="carousel-inner">
 
 									{imgArray.map((item, i) => {
-										
+
 										return (
-											<div key={i} className={`carousel-item  data-bs-interval="10" ${i == 0 ? 'active' : ''}`}>
-												<img src={item} className=" object-fit-contain d-block w-100 " alt={item.split("/")[1]} style={{height:"220px"}} />
+											<div key={i} className={`carousel-item  data-bs-interval="1" ${i == 0 ? 'active' : ''}`}>
+												<img src={item} className=" object-fit-contain d-block w-100 " alt={item.split("/")[1]} style={{ height: "220px" }} />
 											</div>
 
 										);
 									})}
 
 								</div>
-								<button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+								<button className="carousel-control-prev" type="button" data-bs-target="#programmingLanguage" data-bs-slide="prev">
 									<span className="carousel-control-prev-icon" aria-hidden="true"></span>
 									<span className="visually-hidden">Previous</span>
 								</button>
-								<button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+								<button className="carousel-control-next" type="button" data-bs-target="#programmingLanguage" data-bs-slide="next">
 									<span className="carousel-control-next-icon" aria-hidden="true"></span>
 									<span className="visually-hidden">Next</span>
 								</button>
