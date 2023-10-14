56db87986ab33540b6bc0a920f76385a19295da7
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-10-14 13:57:00 +0000

    navbar toggler closes on click

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,31 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/index.css"
 import avocado from "../../assets/img/webpage/avo.png"
 import { Link } from "react-router-dom";
 
 const resumeLink = "https://docs.google.com/document/d/1I-mOHTXc_QFSnbGZAiFSDvcoQgbyPKpg1Sa6f8YrI0w/edit?usp=sharing"
 const Navbar = () => {
+ const handleToggler = ()=>{
+  const navbar = document.querySelector(".navbar-collapse")
+  if(navbar.classList.contains("show")){
+    navbar.classList.remove("show");
+  }
+ }
   return (
     <nav className=" navbar navbar-expand-lg mb-md-4 mb-1 ">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           <img src={avocado} alt="avocado" />
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <Link to="/">
+          <div className="navbar-nav" >
+            <Link onClick={handleToggler} to="/">
               <p className="nav-link active" aria-current="page" href="#">Home</p>
             </Link>
-            <Link to="projects/">
+            <Link onClick={handleToggler} to="projects/">
               <p className="nav-link" href="#">Projects</p>
             </Link>
-            <Link  to={resumeLink} target="_blank">
+            <Link onClick={handleToggler}  to={resumeLink} target="_blank">
             <p className="nav-link" >Resume</p>
             </Link>
-            <Link to="/about">
+            <Link onClick={handleToggler} to="/about">
               <p className="nav-link" href="#">About</p>
             </Link>
 
