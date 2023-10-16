07f1c9e890b23c59a06d7590ac3d897514f4057b
Alex Ayala Palacin <95265085+aayalapalacin@users.noreply.github.com>
2023-10-16 17:17:57 +0000

    toggle and x in navbar mobile

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 const resumeLink = "https://docs.google.com/document/d/1I-mOHTXc_QFSnbGZAiFSDvcoQgbyPKpg1Sa6f8YrI0w/edit?usp=sharing"
 const Navbar = () => {
+  const [toggleOrX, setToggleOrX]= useState(false)
  const handleToggler = ()=>{
   const navbar = document.querySelector(".navbar-collapse")
   if(navbar.classList.contains("show")){
@@ -19,8 +20,9 @@ const Navbar = () => {
             <img src={avocado} alt="avocado" />
           </a>
         </Link>
-        <button className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+        <button onClick={()=> setToggleOrX(!toggleOrX)} className="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          {toggleOrX ? "X" : <span className="navbar-toggler-icon"></span>}
+
         </button>
         <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
           <div className="navbar-nav" >
