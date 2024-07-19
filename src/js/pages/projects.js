1876374e5e0c0fc35aead5be78b9b85903776f29
avokeys <alexander.ayala.palacin@gmail.com>
2024-07-19 20:10:39 +0000

    deleted project and corresponding images

--- src/js/pages/projects.js
+++ src/js/pages/projects.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from "react";
 import benchcrop from "../../assets/img/portrait/benchcrop.jpg"
 // project img
-import archive from "../../assets/img/projects/archive.png"
-import ambee from "../../assets/img/projects/ambee.jpg"
 import schedule_shifter from "../../assets/img/projects/schedule_shifter.png"
 import schedule_shifter_mobile from "../../assets/img/projects/schedule_shifter_mobile.png"
-import cna from "../../assets/img/projects/cna.png"
-import cnaMobile from "../../assets/img/projects/cnaMobile.png"
-import skillshop from "../../assets/img/projects/skillshop.png"
-import skillshopMobile from "../../assets/img/projects/skillshopMobile.png"
+
 import portfolio from "../../assets/img/projects/portfolio.png"
 import portfolioMobile from "../../assets/img/projects/portfolioMobile.png"
 import "../../styles/projects.css"
@@ -18,34 +13,16 @@ let projectContent = [
 		"id": 0,
 		"title": "Schedule Shifter",
 		"description": "Website that dynamically gives visual representation of programs start/end times in a week, name, and description ",
-		"languages": ["REACT", "POSTGRESQL", "BOOTSTRAP","HTML", "CSS",  "JAVASCRIPT"],
+		"languages": ["REACT", "POSTGRESQL", "BOOTSTRAP", "HTML", "CSS", "JAVASCRIPT"],
 		"link": "https://sample-service-name-iewa.onrender.com/",
 		"code": "https://github.com/aayalapalacin/shifting_schedule",
-		"photo": <img className="projectPhoto d-none d-md-block " src={schedule_shifter} alt="schedule_shifter" style={{ objectFit: " contain",height:"56%" }} />,
+		"photo": <img className="projectPhoto d-none d-md-block " src={schedule_shifter} alt="schedule_shifter" style={{ objectFit: " contain", height: "56%" }} />,
 		"photoMobileView": <img className="projectPhoto mt-5 d-block d-md-none w-100" src={schedule_shifter_mobile} alt="schedule_shifter_mobile" style={{ objectFit: " contain" }} />
 	},
+
+
 	{
 		"id": 1,
-		"title": "The Archive",
-		"description": "4th place winner submission for 1 week hackathon to find solution for online shoppers not finding the right fitting jeans. Came in 4th overall place providing unique solution using Levi's database and creating algorithm to determine best fit",
-		"languages": ["HTML", "CSS", "REACT", "BOOTSTRAP", "JAVASCRIPT", "NPM PROGRESS BAR"],
-		"link": "https://levis-the-archive.vercel.app/",
-		"code": "https://github.com/aayalapalacin/theArchive",
-		"photo": <img className="projectPhoto d-none d-md-block  h-100" src={archive} alt="archive" style={{ objectFit: " contain" }} />,
-		"photoMobileView": <img className="projectPhoto mt-5 d-block d-md-none w-100" src={archive} alt="archive" style={{ objectFit: " contain" }} />
-	},
-	{
-		"id": 2,
-		"title": "Ambee",
-		"description": "Mobile app to help users find new movie to watch based on genre, process of elimination with tinder swiping feature, and movies available on netflix via Movies of the Night API.",
-		"languages": ["REACT NATIVE", "MOVIES OF THE NIGHT API", "REACT NATIVE PAPER", "EXPO GO"],
-		"link": "https://expo.dev/@alexayala91/Ambee?serviceType=classic&distribution=expo-go",
-		"code": "https://github.com/aayalapalacin/Ambee",
-		"photo": <img className="projectPhoto d-none d-md-block h-100" src={ambee} alt="ambee" style={{ objectFit: " contain" }} />,
-		"photoMobileView": <img className="projectPhoto w-100  mt-3 d-block d-md-none" src={ambee} alt="ambee" style={{ objectFit: " contain" }} />
-	},
-	{
-		"id": 3,
 		"title": "Portfolio",
 		"description": "Portfolio page showing projects in dynamic layout using react.js, bootstrap, and responsive styling",
 		"languages": ["REACT", "BOOTSTRAP", "HTML", "JAVASCRIPT", "NPM REACT X-ARROWS", "NPM H-5 AUDIO PLAYER"],
@@ -54,26 +31,8 @@ let projectContent = [
 		"photo": <img className="projectPhoto d-none d-md-block" src={portfolio} alt="portfolio" style={{ height: "46%", objectFit: " contain" }} />,
 		"photoMobileView": <img className="projectPhoto mt-5 d-block d-md-none w-100" src={portfolioMobile} alt="portfolio mobile" style={{ objectFit: " contain" }} />
 	},
-	{
-		"id": 4,
-		"title": "Miami Dade College CNA ",
-		"description": "Miami Dade College website for nursing students executed with clean and readable code in HTML. Clean and professional look with bootstrap and CSS styling, and dynamic with media queries for all screen sizes",
-		"languages": ["HTML", "CSS", "JQUERY", "BOOTSTRAP", "JAVASCRIPT"],
-		"link": "https://ce.mdc.edu/contentManagement.do?method=load&code=CM000007",
-		"code": "n/a",
-		"photo": <img className="projectPhoto d-none d-md-block " src={cna} alt="cna" style={{ height: "56%", objectFit: " contain" }} />,
-		"photoMobileView": <img className="projectPhoto mt-5 d-block d-md-none w-100" src={cnaMobile} alt="cna mobile" style={{ objectFit: " contain" }} />
-	},
-	{
-		"id": 5,
-		"title": "Miami Dade College Skillshop ",
-		"description": "Miami Dade College website for enrolled sign up for courses. executed with clean and readable code in HTML. Clean and professional look with bootstrap and CSS styling, and dynamic with media queries for all screen sizes.",
-		"languages": ["HTML", "CSS", "JQUERY", "BOOTSTRAP", "JAVASCRIPT"],
-		"link": "https://www.mdc.edu/skillshop/",
-		"code": "n/a",
-		"photo": <img className="projectPhoto d-none d-md-block " src={skillshop} alt="skillshop" style={{ height: "65%", objectFit: " contain" }} />,
-		"photoMobileView": <img className="projectPhoto mt-5 d-block d-md-none w-100" src={skillshopMobile} alt="skillshop mobile" style={{ objectFit: " contain" }} />
-	},
+
+
 ]
 const Projects = () => {
 	const [active, setActive] = useState(0)
@@ -291,7 +250,7 @@ const Projects = () => {
 													{/* title mobile */}
 													<p className=" d-block d-md-none me-2 title projectText fw-bold">Github: </p>
 													<p className={`projectText mb-2 mb-md-1 ${project.id == active ? 'animation-up' : ''}`}>
-													{project.code != "n/a" ?
+														{project.code != "n/a" ?
 															<a
 																href={project.code}
 																target="_blank"
@@ -304,12 +263,12 @@ const Projects = () => {
 														}
 													</p>
 												</div>
-											
+
 											</div>
-											<div 
-											className={`project-content-photo text-center align-items-md-center align-items-start d-flex justify-content-center col-6 ${project.id == active ? 'animation-fade' : ''}`}
-											 style={{ height: "394px" }}
-											 >
+											<div
+												className={`project-content-photo text-center align-items-md-center align-items-start d-flex justify-content-center col-6 ${project.id == active ? 'animation-fade' : ''}`}
+												style={{ height: "394px" }}
+											>
 												{project.photo}
 												{project.photoMobileView}
 											</div>
